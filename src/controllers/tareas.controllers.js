acf70f27870dc3b09a80cd4d94ccf450fd0762d3
Majuanado <crismaldoalf@gmail.com>
2022-10-12 16:05:16 -0300

    Creación de controlador put

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -4,41 +4,81 @@
 //se requiere el modelo de tareas de la carpeta de modelos, el cual ya debe estar hecho....y se la guarda en una variable 
 
 const ColeccionTareas = require('../models/models.tareas');
-
+const ModeloUsuario = require('../models/models.usuarios');
 ctrlTareas = {};
 
+
 ctrlTareas.getTareas = async (req,res)=>{
-    const tareas = await ColeccionTareas.find()
-    return res.json({
-        message:`El numero de tareas son: ${tareas.length}`,
-        tareas
-    })
-}
+    try {
+        const tareas = await ColeccionTareas.find({activa: true})
+        return res.json({
+            message:`El numero de tareas son: ${tareas.length}`,
+            tareas
+        })
+    } catch (error) {
+        return res.json("No se encontraron las tareas")
+    }
+};
+
+ctrlTareas.getTareas_User = async (req,res) => {
+    try {
+        const idUsua = req.user._id;
+        const tareasUsu =  await ColeccionTareas.find({$and:[{idUsua},{activa: true}]})
+        if(!tareasUsu.length){
+            return res.json("No se encontraron tareas")
+        }
+        return res.json({tareasUsu})
+        
+    } catch (error) {
+        return res.json("Error al obtener las tareas")
+    }
+};
 
 ctrlTareas.postTareas = async (req,res)=>{
-    const {persona_encargada,descripcion,estado}=req.body;
-    //instanciado...
-    const newTarea = new ColeccionTareas({
-        persona_encargada,
-        descripcion,
-        estado
-    })
-    const tareabd = await newTarea.save();
-    return res.json({
-        message:'Tarea cargada a la base de datos',
-        tareabd
-    })
-}
+    try {
+        const {persona_encargada,descripcion,estado}=req.body;
+        //instanciado...
+        const newTarea = new ColeccionTareas({
+            persona_encargada,
+            descripcion,
+            estado
+        })
+    
+        const tareabd = await newTarea.save();
+        return res.json({
+            message:'Tarea cargada a la base de datos',
+            tareabd
+        })
+    } catch (error) {
+        return res.json('Error para crear la tarea')
+    }
+};
 
 ctrlTareas.putTareas = async (req,res)=>{
-    
-    const id = req.params.id;
-    const {persona_encargada,descripcion,estado}=req.body;
+    try {
+        const idTarea = req.params.idTarea;
+        const idUsuar = req.user._id;
 
-    const tareaUpdate = await ColeccionTareas.findByIdAndUpdate(id,{persona_encargada,descripcion,estado});
-    console.log(tareaUpdate);
+        const {persona_encargada,descripcion,estado}=req.body;
+        
+        if(!idTarea){
+            return res.json("Falta información necesaria")
+        }
 
-    return res.json(await ColeccionTareas.find({"_id":id}));
-}
+        const idTareaString = idTarea.toString();
+        const idUsuaString = idUsuar.toString();
+
+        if((idTareaString===idUsuaString)||req.user.isAdmin === true){
+            const tareaUpdate = await ColeccionTareas.findByIdAndUpdate(idTarea,{persona_encargada,descripcion,estado});
+            console.log(tareaUpdate);
+        
+            return res.json(await ColeccionTareas.find({"_id":idTarea}));
+        }
+
+    } catch (error) {
+        res.json('No se pudo actualizar la tarea')
+    }
+};
+
 module.exports = ctrlTareas;
 
