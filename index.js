6e3ffba22518f55c0e29659912e10209947a5112
Majuanado <crismaldoalf@gmail.com>
2022-10-13 13:44:42 -0300

    Reacomodación index

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const app = express();
+
 const cors = require('cors'); //estandariza la aplicación para q sea aceptada por todos los navegadores
 const morgan = require('morgan'); //para ver las peticiones e ipes por consola
 require('dotenv').config();
+
 //CONEXION
 const coneccionbd = require('./src/conexiones')
 coneccionbd();
+
 //middlewares
 app.use(express.json());
 app.use(cors());
 app.use(morgan('tiny'));
 
-const puerto = 4000;
-
 app.use(require('./src/routes/tareas.routes'));
 app.use(require('./src/routes/usuarios.routes'));
 app.use(require('./src/routes/authen.routes'));
 
-
+const puerto = 4000;
 app.listen(puerto, ()=>{
     console.log(`Servidor iniciado en el puerto ${puerto}`);
 })
